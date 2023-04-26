b204f4a5ab10a37efca5da250c02e169fd0f51c4
pj <1242201332@qq.com>
2023-04-26 16:29:10 +0800

    广播混用

--- src/mixins/emitter.js
+++ src/mixins/emitter.js
@@ -1,7 +1,15 @@
+/**
+ * 广播方法定义
+ * @param {String} componentName 组件名称
+ * @param {String} eventName 事件名称
+ * @param {Object} params  参数
+ */
 function broadcast(componentName, eventName, params) {
+  //遍历组件中的子组件
   this.$children.forEach(child => {
+    //vm.$options 用于当前 Vue 实例的初始化选项。需要在选项中包含自定义 property 时会有用处：
     var name = child.$options.componentName;
-
+    //递归确保每一个孙和子组件都能接收到
     if (name === componentName) {
       child.$emit.apply(child, [eventName].concat(params));
     } else {
@@ -11,6 +19,12 @@ function broadcast(componentName, eventName, params) {
 }
 export default {
   methods: {
+    /**
+ * 向父爷传播方法定义
+ * @param {String} componentName 组件名称
+ * @param {String} eventName 事件名称
+ * @param {Object} params  参数
+ */
     dispatch(componentName, eventName, params) {
       var parent = this.$parent || this.$root;
       var name = parent.$options.componentName;
