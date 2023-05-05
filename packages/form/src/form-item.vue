1cfb639e9f815f2ad61baaeccda450d21ad8b30b
pj <1242201332@qq.com>
2023-05-05 17:13:37 +0800

    form注释

--- packages/form/src/form-item.vue
+++ packages/form/src/form-item.vue
@@ -40,7 +40,7 @@
 </template>
 <script>
   import AsyncValidator from 'async-validator';
-  import emitter from 'element-ui/src/mixins/emitter';
+  import emitter from 'element-ui/src/mixins/emitter';//组件下发与向上传递方法
   import objectAssign from 'element-ui/src/utils/merge';
   import { noop, getPropByPath } from 'element-ui/src/utils/util';
   import LabelWrap from './label-wrap';
